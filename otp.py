0b61c143d0f5cbb020c1808c7fadcba44f8fe402
woleekanola <woleekanola@gmail>
2022-03-19 08:09:23 +0100

    initial commit

---
+++ otp.py
@@ -0,0 +1,18 @@
+import random
+# if there is a database fetch recent OTP codes from DB and populate otpees array
+otpees = []
+
+# create a function that generates random otp of any lenght
+def otpGenerator(length):
+    otp = []
+    for x in range(length):
+      otp.append(str(int(random.random() * 10)))
+    print(otp)
+    curOTP = ''.join(otp)
+    otpees.append(curOTP)
+    return curOTP
+
+
+print("welcome to Random OTP Generator")
+length = input('How Many digits do you want the otp to have')
+print(otpGenerator(int(length)))
---
+++ pwrdgen.py
@@ -0,0 +1,2 @@
+# create a password generator function that accepts several constraints as aguements 
+#and returns a pword that conforms to these constraints  
